35ebbd3873f03304122891f1756f4ffe5369d2cb
prash2511 <prashantgalgunde@gmail.com>
2022-07-14 15:29:13 +0530

    Console app added

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace consoleapp4;
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Trainee: Everybody here saying we'll get terminated");
+
+        Console.WriteLine("Developer: Welcome to LTI!");
+    }
+}
